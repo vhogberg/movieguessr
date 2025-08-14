128b6d253297a69d4e85dc629372167124229d38
vhogberg <119532259+vhogberg@users.noreply.github.com>
2025-08-14 19:09:45 +0200

    Fixed attribution

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,20 +5,19 @@ import TMDBLogo from "../../assets/TMDB-logo.svg";
 export default function Footer() {
   return (
     <footer>
-      <div className="TMDB-attribution">
-        <p>
-          This product uses the{" "}
-          <a
-            href="https://www.themoviedb.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            TMDB API
-          </a>{" "}
-          but is not endorsed or certified by TMDB.
-        </p>
-        <img src={TMDBLogo} alt="TMDB-logo" className="TMDB-logo" />
-      </div>
+      <p>
+        This product uses the{" "}
+        <a
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TMDB API
+        </a>{" "}
+        but is not endorsed or certified by TMDB.
+      </p>
+      <img src={TMDBLogo} alt="TMDB-logo" className="TMDB-logo" />
+    
       <p>© 2025 MovieGuessr. All rights reserved.</p>
     </footer>
   );
